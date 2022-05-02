88db868af3deab7f835cd0a2f0c37f7bfc1b751d
cristianchajon04 <101077981+cristianchajon04@users.noreply.github.com>
2022-05-02 17:55:05 -0600

    Add files via upload

---
+++ Ejercicio12.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Numeros del 20 al 30" << endl;
+    for (int i = 20; i <= 30; i++){
+        cout << i << endl;
+    }
+    system("pause");
+    return 0;
+}
