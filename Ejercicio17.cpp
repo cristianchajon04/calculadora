383460ac982333508f3a9b3433acd032f03b4cfa
cristianchajon04 <101077981+cristianchajon04@users.noreply.github.com>
2022-05-02 18:06:02 -0600

    Add files via upload

---
+++ Ejercicio17.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x = 50;
+    cout << "Lista de numeros del 50 al 80" << endl;
+    while (x <= 80){
+        cout << x << endl;
+        x++;
+    }
+    system("pause");
+    return 0;
+}
